4f37ee812d309a44dbe2273d014ffece10f2a34e
François KUBLER <francois+github@kubler.org>
2025-08-11 11:15:35 +0200

    feat(env): update profile.d script

--- files/profile.d/050-grafana.sh
+++ files/profile.d/050-grafana.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-export PATH="${PATH}:${HOME}/bin"
+# Update PATH:
+PATH="${PATH}:${HOME}/bin"
+
+# Grafana does NOT support 'sslmode=prefer', replace with 'sslmode=require':
+GF_DATABASE_URL="${SCALINGO_POSTGRESQL_URL/prefer/require}"
+
+export PATH
+export GF_DATABASE_URL
